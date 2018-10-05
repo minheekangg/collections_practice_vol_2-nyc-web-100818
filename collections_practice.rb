fd59dca16a8dd6a2c9a32bb3a74a8e12bc9282e9
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 17:07:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,81 @@
 # your code goes here
+def begins_with_r(array)
+  array.each do |word|
+    if word[0] != "r"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  empty = []
+  array.each do |word|
+    if word.include?("a")
+        empty << word
+      end
+  end
+  return empty
+end
+
+def first_wa(array)
+  array.each do |word|
+    if word[0..1] == "wa"
+      return word
+    end
+  end
+end
+
+def remove_non_strings(array)
+  empty = []
+  array.each do |word|
+    if word == word.to_s
+      empty << word
+    end
+  end
+  return empty
+end
+
+def count_elements(array)
+  grouped = array.group_by {|el| el[:name]}
+  return grouped.map {|k,v| {:name => k,:count => v.length}}
+end
+
+
+def merge_data(keys, data)
+  keys.each do |k,v|
+    data.each do |name, data|
+      if k == name
+        puts k
+      end
+      end
+  end
+  return data
+end
+
+def find_cool(array)
+  cool = []
+  array.each do |element|
+  if element[:temperature] == "cool"
+    cool << element
+  end
+end
+  return cool
+end
+
+
+def organize_schools(array)
+  h={}
+  h2={}
+  h = array.group_by do |k,v|
+    v[:location]
+  end
+  h.each do |city, data|
+    curr_school = []
+    data.each do |school, delete|
+      curr_school<<school
+    end
+    h2[city] = curr_school
+  end
+  return h2
+end
