4373cb8af6d73102548f8eb3444e92a683575bb9
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 17:24:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,7 +49,7 @@ def merge_data(keys, data)
     data.flatten.each do |name, needed_data|
       name.each do |key, value|
       if key_name == key
-        puts needed_data
+        puts value
         puts lower_keys
         new_hash = name.merge(lower_keys)
       end
